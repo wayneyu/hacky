5ed6ae341e8254d97d696f28b7f724c400d98141
Kim, Lucy <lucyyrkim@gmail.com>
2018-01-09 11:29:48 -0500

    layout try

--- hacky-ui/src/app/app.module.ts
+++ hacky-ui/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { FundListComponent }   from './fund-list/fund-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FundListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
