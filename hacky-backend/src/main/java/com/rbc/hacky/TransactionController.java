d3b3286a3042cc2e9c809dabfde1a352cd273b16
bohao <bohaowang@gmail.com>
2018-01-09 13:31:31 -0500

    Added spring service

--- hacky-backend/src/main/java/com/rbc/hacky/TransactionController.java
+++ hacky-backend/src/main/java/com/rbc/hacky/TransactionController.java
@@ -2,14 +2,13 @@ package com.rbc.hacky;
 
 import com.google.common.collect.Lists;
 import com.rbc.hacky.model.Transaction;
-import com.rbc.hacky.model.TransactionRepository;
+import com.rbc.hacky.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,16 +19,15 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @RequestMapping("/transactions")
 public class TransactionController {
 
-    private final TransactionRepository transactionRepository;
+    private final TransactionService transactionRepository;
 
     @Autowired
-    public TransactionController(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
+    public TransactionController(TransactionService transactionService) {
+        this.transactionRepository = transactionService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public HttpEntity<List<Transaction>> greeting(
-            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+    public HttpEntity<List<Transaction>> greeting() {
 
         Iterable<Transaction> transactionList = transactionRepository.findAll();
         transactionList.forEach(transaction -> transaction.add(linkTo(TransactionController.class).slash(transaction.getTransactionId()).withSelfRel()));
