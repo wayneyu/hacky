831ff636607b04b8355eda4b7d39aceb4e080338
bohao <bohaowang@gmail.com>
2018-01-12 14:18:50 -0500

    add login user

--- hacky-ui/src/app/app.component.ts
+++ hacky-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Globals} from './globals'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fund Sales Analytics Platform';
+
+  constructor(private globals: Globals){
+
+  }
 }
